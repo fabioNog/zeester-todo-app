0cd09f821e73c03456b8b1e53c5bd45d9f122445
Fabio <fabio.carmo@computacao.ufla.br>
2017-09-12 22:51:00 -0300

    fixing app.css dependencie

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
-
     <div>
         <PageHeader name='Sobre' small='Nós'></PageHeader>
         <h2>Nossa História</h2>
--- frontend/src/template/iconButton.jsx
+++ frontend/src/template/iconButton.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
-import If from '../template/if'
+import If from './if'
 
-export default props => {
-    if (props.hide) {
-        return null
-    } else {
-        return (
-            <If test={!props.hide}>
-                <button className={'btn btn-' + props.style} onClick={props.onClick}>
-                    <i className={'fa fa-' + props.icon}></i>
-                </button>
-            </If>
-        )
-    }
-}
+export default props => (
+    <If test={!props.hide}>
+        <button className={'btn btn-' + props.style}
+            onClick={props.onClick}>
+            <i className={'fa fa-' + props.icon}></i>
+        </button>
+    </If>
+)
